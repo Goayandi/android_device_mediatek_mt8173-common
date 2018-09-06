2965ec2e77d39885e71486f7afa5073d3e7ae187
goayandi <r.borutha@gmail.com>
2018-09-06 19:21:03 +0200

    sepolicy: reverting changes Moving back to mt8173 and common sepolicy.

--- mt8173.mk
+++ mt8173.mk
@@ -189,8 +189,8 @@ BOARD_SEPOLICY_DIRS := \
     $(COMMON_PATH)/common/sepolicy/basic \
 		$(COMMON_PATH)/mt8173/sepolicy/basic \
 #    $(COMMON_PATH)/common/sepolicy/bsp \
-    $(COMMON_PATH)/common/sepolicy/full \
-    $(COMMON_PATH)/mt8173/sepolicy/basic \
+#    $(COMMON_PATH)/common/sepolicy/full \
+#    $(COMMON_PATH)/mt8173/sepolicy/basic \
 
 # Android Debugging
 #ADDITIONAL_DEFAULT_PROPERTIES += \
