b4b2703847c0fe3d6f7f382e7e123b61ba6cb3b5
goayandi <r.borutha@gmail.com>
2018-09-12 23:14:14 +0200

    First upload: error while building > Some errors while building lineage-14.1 and rr-nougat. Looks like something is wrong with lexer (?)

--- mt8173.mk
+++ mt8173.mk
@@ -17,54 +17,71 @@ TARGET_MEDIATEK_MT8173-COMMON := true
 
 COMMON_PATH := device/mediatek/mt8173-common
 
-# Common overlays
+# Overlays
 DEVICE_PACKAGE_OVERLAYS += $(COMMON_PATH)/overlay
 
-# Configs
-PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml \
-    $(COMMON_PATH)/prebuilt/etc/ecc_list.xml:system/etc/ecc_list.xml \
-    $(COMMON_PATH)/prebuilt/etc/spn-conf.xml:system/etc/spn-conf.xml
+# Headers
+# TARGET_SPECIFIC_HEADER_PATH := $(COMMON_PATH)/include
 
-# WiFi
-PRODUCT_PACKAGES += \
-		    dhcpcd.conf \
-		    hostapd \
-		    libwpa_client \
-		    wpa_supplicant \
-		    wpa_supplicant.conf \
-		    lib_driver_cmd_mt66xx
-
-PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/prebuilt/etc/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
-    $(COMMON_PATH)/prebuilt/etc/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
-    $(COMMON_PATH)/prebuilt/etc/wifi/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf
-
-# Messaging
-PRODUCT_PACKAGES += \
-    messaging \
-    Stk
-
-# Display
-PRODUCT_PACKAGES += \
-    libnl_2 \
-    libion
-
-BACKLIGHT_PATH := /sys/class/leds/lcd-backlight/brightness
-
-# Audio
-BOARD_USES_MTK_AUDIO := true
+#### Packages ####
 
 # Audio
 PRODUCT_PACKAGES += \
-    audio.a2dp.default \
     audio.r_submix.default \
+    audio.a2dp.default \
+    audio.usb.default \
     libaudio-resampler \
     libtinyalsa \
     libtinycompress \
     libtinymix \
     libtinyxml
 
+# Charger
+PRODUCT_PACKAGES += \
+    charger_res_images
+
+# Curl
+PRODUCT_PACKAGES += \
+    libcurl
+
+# Display
+PRODUCT_PACKAGES += \
+    libion
+
+# Doze
+PRODUCT_PACKAGES += \
+    Doze
+
+# Misc
+PRODUCT_PACKAGES += \
+    librs_jni \
+    libnl_2
+
+# Snap
+PRODUCT_PACKAGES += \
+   Snap
+
+# Symbols
+PRODUCT_PACKAGES += \
+    libshim_ui \
+    libshim_gui \
+    libshim_ifc \
+    libshim_misc
+
+# USB
+PRODUCT_PACKAGES += \
+    com.android.future.usb.accessory
+
+# Wifi
+PRODUCT_PACKAGES += \
+    libwpa_client \
+    hostapd \
+    wpa_supplicant \
+    wpa_supplicant.conf
+
+#### Copy files ####
+
+# Audio
 PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:/system/etc/audio_policy_volumes.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:/system/etc/default_volume_tables.xml \
@@ -77,97 +94,120 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:system/etc/media_codecs_google_video_le.xml
 
-# Media
-PRODUCT_PROPERTY_OVERRIDES += \
-		    media.stagefright.legacyencoder=0
-
-# IO Scheduler
-PRODUCT_PROPERTY_OVERRIDES += \
-				    sys.io.scheduler=cfq
-
 # GPS
 PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/prebuilt/etc/agps_profiles_conf2.xml:system/etc/agps_profiles_conf2.xml
-
-# Filesystem management tools
-PRODUCT_PACKAGES += \
-    e2fsck \
-    fsck.f2fs \
-    mkfs.f2fs \
-    make_ext4fs
+     $(COMMON_PATH)/configs/agps_profiles_conf2.xml:system/vendor/etc/agps_profiles_conf2.xml
 
 # Permissions
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
-    frameworks/native/data/etc/android.hardware.camera.autofocus.xml:system/etc/permissions/android.hardware.camera.autofocus.xml \
-    frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
-    frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
+    frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
-    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
-    frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
-    frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.compass.xml \
     frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
-    frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
     frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.compass.xml \
     frameworks/native/data/etc/android.hardware.sensor.stepcounter.xml:system/etc/permissions/android.hardware.sensor.stepcounter.xml \
     frameworks/native/data/etc/android.hardware.sensor.stepdetector.xml:system/etc/permissions/android.hardware.sensor.stepdetector.xml \
-    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+    frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.xml \
     frameworks/native/data/etc/android.hardware.touchscreen.xml:system/etc/permissions/android.hardware.touchscreen.xml \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+    frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
+    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+    frameworks/native/data/etc/android.hardware.vr.high_performance.xml:system/etc/permissions/android.hardware.vr.high_performance.xml \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
-    frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
-    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
-    frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml \
-    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
-    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml
+    frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+    frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+    frameworks/native/data/etc/android.hardware.camera.manual_sensor.xml:system/etc/permissions/android.hardware.camera.manual_sensor.xml \
+    frameworks/native/data/etc/android.hardware.camera.autofocus.xml:system/etc/permissions/android.hardware.camera.autofocus.xml
 
-# exFAT
-PRODUCT_PACKAGES += \
-    mount.exfat \
-    fsck.exfat \
-    mkfs.exfat
+# Ramdisk
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,${LOCAL_PATH}/rootdir,root)
 
-# NTFS
-PRODUCT_PACKAGES += \
-    fsck.ntfs \
-    mkfs.ntfs \
-    mount.ntfs
+# Telephony
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/configs/ecc_list.xml:system/etc/ecc_list.xml \
+#   $(COMMON_PATH)/configs/apns-conf.xml:system/etc/apns-conf.xml \
+#   $(COMMON_PATH)/configs/spn-conf.xml:system/etc/spn-conf.xml
 
-# USB
-PRODUCT_PACKAGES += \
-    com.android.future.usb.accessory
+# WIFI
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/configs/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
+    $(COMMON_PATH)/configs/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
+    $(COMMON_PATH)/configs/wifi/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf
 
-# WallpaperPicker
-PRODUCT_PACKAGES += \
-    WallpaperPicker
+#### Props ####
 
-# Sensor Calibration
-PRODUCT_PACKAGES += \
-    libem_sensor_jni
+# Android Debugging
+ADDITIONAL_DEFAULT_PROPERTIES += \
+    ro.secure=0 \
+    ro.adb.secure=0
+
+#### BoardConfigs ####
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+
+# Boot animation
+TARGET_BOOTANIMATION_MULTITHREAD_DECODE := true
+
+# Camera
+USE_CAMERA_STUB := true
 
 # Charger
-PRODUCT_PACKAGES += \
-    charger \
-    charger_res_images
+BACKLIGHT_PATH := /sys/class/leds/lcd-backlight/brightness
 
 # Common Properties
 TARGET_SYSTEM_PROP := $(COMMON_PATH)/system.prop
 
-# Key Layouts
-PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/keylayouts/ACCDET.kl:system/usr/keylayout/ACCDET.kl \
-    $(COMMON_PATH)/keylayouts/mtk-kpd.kl:system/usr/keylayout/mtk-kpd.kl
+# Cyanogenmod H/W Hooks
+BOARD_USES_CYANOGEN_HARDWARE := true
+BOARD_HARDWARE_CLASS := $(COMMON_PATH)/cmhw
 
-# Power
-PRODUCT_PACKAGES += \
-    power.default \
-    power.mt8173
+# Dalvik Tweak
+PRODUCT_TAGS += dalvik.gc.type-precise
 
-# Wireless
-BOARD_WLAN_DEVICE := MediaTek
+# Dexpreopt
+WITH_DEXPREOPT := true
+DONT_DEXPREOPT_PREBUILTS := true
+
+# Disable memcpy opt (for audio libraries)
+TARGET_CPU_MEMCPY_OPT_DISABLE := true
+
+# Display
+USE_OPENGL_RENDERER := true
+TARGET_FORCE_HWC_FOR_VIRTUAL_DISPLAYS := true
+NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
+TARGET_RUNNING_WITHOUT_SYNC_FRAMEWORK := true
+
+# Enable Minikin text layout engine (will be the default soon)
+USE_MINIKIN := true
+
+# Light HAL
+TARGET_PROVIDES_LIBLIGHT := true
+
+# MTK Hardware
+BOARD_USES_MTK_HARDWARE := true
+
+# RIL
+BOARD_RIL_CLASS := ../../../$(COMMON_PATH)/ril
+
+# Seccomp filter
+BOARD_SECCOMP_POLICY := $(COMMON_PATH)/seccomp
+
+# SELinux
+BOARD_SEPOLICY_DIRS := $(COMMON_PATH)/sepolicy
+
+# Wifi
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 BOARD_HOSTAPD_DRIVER := NL80211
 BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_mt66xx
@@ -177,42 +217,3 @@ WIFI_DRIVER_FW_PATH_PARAM := /dev/wmtWifi
 WIFI_DRIVER_FW_PATH_AP := AP
 WIFI_DRIVER_FW_PATH_STA := STA
 WIFI_DRIVER_FW_PATH_P2P := P2P
-WIFI_DRIVER_STATE_CTRL_PARAM := /dev/wmtWifi
-WIFI_DRIVER_STATE_ON := 1
-WIFI_DRIVER_STATE_OFF := 0
-
-# Bluetooth
-BOARD_HAVE_BLUETOOTH := true
-
-# Boot animation
-TARGET_BOOTANIMATION_MULTITHREAD_DECODE := true
-
-# Charger
-BOARD_CHARGER_SHOW_PERCENTAGE := true
-
-# Seccomp filter
-BOARD_SECCOMP_POLICY := $(COMMON_PATH)/seccomp
-
-# SELinux
-BOARD_SEPOLICY_DIRS := \
-    $(COMMON_PATH)/common/sepolicy/basic \
-    $(COMMON_PATH)/common/sepolicy/bsp \
-    $(COMMON_PATH)/common/sepolicy/full \
-    $(COMMON_PATH)/mt8173/sepolicy/basic \
-
-# Android Debugging
-ADDITIONAL_DEFAULT_PROPERTIES += \
-    ro.secure=0 \
-    ro.adb.secure=0
-
-ifneq ($(TARGET_BUILD_VARIANT), user)
-
-# Mediatek logging service
-PRODUCT_PACKAGES += \
-    MTKLogger \
-    emdlogger1 \
-    mdlogger \
-    mobile_log_d \
-    netdiag \
-    tcpdump
-endif
